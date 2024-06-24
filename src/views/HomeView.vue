47f346b4ac7459c33a59d7708f5c0d3c55301d7c
THEkingmay <beerranee@gmail.com>
2024-06-24 14:16:02 +0700

    respondsive commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -90,7 +90,7 @@ function clickdeleteresult(result){
     </div>
     <div class=" flex flex-col p-4">
         <div class="mb-2 border-b" v-for="(task, index) in tasks" :key="index">
-            <div class="grid grid-cols-2">
+            <div class="flex flex-col items-start md:grid grid-cols-2">
                 <div class="p-3">
                     <div class="flex items-center">
                         <input type="checkbox" class="checkbox checkbox-success mr-4" v-model="task.success" @change="successChange(index)">
@@ -100,7 +100,7 @@ function clickdeleteresult(result){
                         </button>
                     </div>
                 </div>
-                <div class="flex justify-end items-center">
+                <div class="flex justify-end items-center h-full">
                     <RouterLink :to="{ name:'edit' }" >
                     <button class="btn btn-ghost mr-3" @click="edit(index)" >
                         <svg viewBox="0 0 24 24" fill="none" class="w-5" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M5.5 10C7.433 10 9 8.433 9 6.5C9 4.567 7.433 3 5.5 3C3.567 3 2 4.567 2 6.5C2 8.433 3.567 10 5.5 10Z" stroke="#fffa6b" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M5.5 21C7.433 21 9 19.433 9 17.5C9 15.567 7.433 14 5.5 14C3.567 14 2 15.567 2 17.5C2 19.433 3.567 21 5.5 21Z" stroke="#fffa6b" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M22 6L8.64999 15.98" stroke="#fffa6b" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M22 17.97L8.64999 7.97998" stroke="#fffa6b" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
